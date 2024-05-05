7db83d48f37b2929f07f38cf1cae85f315f6c2c1
zaru6 <markoruza1@gmail.com>
2024-05-05 19:33:32 +0200

    added cors

---
+++ src/main/java/com/evilapp/fire/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.evilapp.fire.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+              .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+              .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+              .allowedHeaders("Content-Type", "Authorization")
+              .exposedHeaders("Authorization")
+              .maxAge(3600);
+    }
+}
